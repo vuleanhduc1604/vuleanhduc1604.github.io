810206e4591ec94217c94c9eecf17ba0268cd51a
unknown <vuleanhduc1604@gmail.com>
2020-07-29 14:55:23 +0700

    main.js

---
+++ Day5/main.js
@@ -0,0 +1,14 @@
+console.log("Hello World")
+var userName
+userName = "name"
+// console.log(userName)
+var firstName = "Nguyễn"
+var lastName = "A"
+var fullName = firstName + " " + lastName
+console.log (fullName)
+var lengthfullName = fullName.length
+console.log (lengthfullName)
+var fullName1 = firstName.concat(" ", lastName)
+console.log (fullName1)
+var position = fullName.indexOf("A")
+console.log(position)
